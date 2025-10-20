a43b231fc62168533130a22dec471acddecfbc08
Hashim Ali Munir <hashimalimunir@gmail.com>
2025-10-20 16:47:00 +0500

    add Kotlin library versions and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
     alias(libs.plugins.kotlin.serialization)
-    `maven-publish`
     `java-library`
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
@@ -35,63 +34,10 @@ kotlin {
     jvmToolchain(21)
 }
 
-// Publishing configuration
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            
-            groupId = "co.hashline"
-            artifactId = "events-kt"
-            version = "1.0"
-            
-            pom {
-                name.set("Events-KT")
-                description.set("A lightweight, coroutine-based event bus library for Kotlin applications")
-                url.set("https://github.com/hash-line/events-kt")
-                
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                
-                developers {
-                    developer {
-                        id.set("hashline")
-                        name.set("Hashline")
-                        email.set("contact@hashline.co")
-                    }
-                }
-                
-                scm {
-                    connection.set("scm:git:git://github.com/hash-line/events-kt.git")
-                    developerConnection.set("scm:git:ssh://github.com:hash-line/events-kt.git")
-                    url.set("https://github.com/hash-line/events-kt")
-                }
-            }
-        }
-    }
-    
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/hash-line/events-kt")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
-}
-
 // Maven Central publishing configuration
 mavenPublishing {
-    // Only sign when running in CI (GitHub Actions)
-    if (System.getenv("CI") == "true") {
-        signAllPublications()
-    }
+    // Always sign all publications
+    signAllPublications()
 
     coordinates("co.hashline", "events-kt", version.toString())
 
